d2264c23b3c434f95a397f909d04cf8a73c07572
Vitor Esposito <vitor.esposito@scaffoldeducation.com.br>
2020-07-31 12:25:21 -0300

    remove env attribute port in config.

--- src/server/config/option.js
+++ src/server/config/option.js
@@ -1,4 +1,4 @@
 module.exports = {
   cdn: process.env.TAURUS_MANAGER_CDN || true,
-  port: process.env.PORT || process.env.TAURUS_MANAGER_PORT || '8001',
+  port: process.env.TAURUS_MANAGER_PORT || '8001',
 };
